5851df20942f37100097bfb8d54bbe698706572d
Andrés Salgado <andressalgado41@gmail.com>
2020-01-15 18:41:55 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# saludo = "Hola "
+# nombre = input("Ingrese su nombre ")
+# print(saludo + nombre)
+
+num1 = int(input("Ingrese el primer número: "))
+num2 = int(input("Ingrese el segundo número: "))
+print("La suma de num1 y num2 es:")
+print(num1+num2)
+print("La resta de num1 y num2 es:")
+print(num1-num2)
+print("La multiplicación de num1 y num2 es:")
+print(num1*num2)
+print("La división de num1 y num2 es:")
+print(num1/num2)
