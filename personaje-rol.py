d0d36db44c94dd2cd01f2538ac4d8fd236879631
Andrés Salgado <andressalgado41@gmail.com>
2020-01-17 10:31:55 -0500

    Personaje de rol

---
+++ personaje-rol.py
@@ -0,0 +1,90 @@
+# Personaje de rol
+'''
+debemos tirar 4 dados y quedarme con la suma de los tres mejores
+'''
+
+import random
+
+dado1 = random.randint(1,6)
+dado2 = random.randint(1,6)
+dado3 = random.randint(1,6)
+dado4 = random.randint(1,6)
+
+suma = dado1 + dado2 + dado3 + dado4
+menor = min (dado1,dado2,dado3,dado4)
+
+res = suma - menor
+
+print("Fuerza %i"% res)
+
+
+
+dado1 = random.randint(1,6)
+dado2 = random.randint(1,6)
+dado3 = random.randint(1,6)
+dado4 = random.randint(1,6)
+
+suma = dado1 + dado2 + dado3 + dado4
+menor = min (dado1,dado2,dado3,dado4)
+
+res = suma - menor
+
+print("Destreza %i"% res)
+
+
+
+dado1 = random.randint(1,6)
+dado2 = random.randint(1,6)
+dado3 = random.randint(1,6)
+dado4 = random.randint(1,6)
+
+suma = dado1 + dado2 + dado3 + dado4
+menor = min (dado1,dado2,dado3,dado4)
+
+res = suma - menor
+
+print("Contitucion %i"% res)
+
+
+
+dado1 = random.randint(1,6)
+dado2 = random.randint(1,6)
+dado3 = random.randint(1,6)
+dado4 = random.randint(1,6)
+
+suma = dado1 + dado2 + dado3 + dado4
+menor = min (dado1,dado2,dado3,dado4)
+
+res = suma - menor
+
+print("Inteligencia %i"% res)
+
+
+
+dado1 = random.randint(1,6)
+dado2 = random.randint(1,6)
+dado3 = random.randint(1,6)
+dado4 = random.randint(1,6)
+
+suma = dado1 + dado2 + dado3 + dado4
+menor = min (dado1,dado2,dado3,dado4)
+
+res = suma - menor
+
+print("Sabiduria %i"% res)
+
+
+
+dado1 = random.randint(1,6)
+dado2 = random.randint(1,6)
+dado3 = random.randint(1,6)
+dado4 = random.randint(1,6)
+
+suma = dado1 + dado2 + dado3 + dado4
+menor = min (dado1,dado2,dado3,dado4)
+
+res = suma - menor
+
+print("Carisma %i"% res)
+
+
